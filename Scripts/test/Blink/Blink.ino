93b301c937be2a1a15837f7a5fb26ed28de5de7c
andxu <andxu@microsoft.com>
2017-03-31 16:43:54 +0800

    typo

--- Scripts/test/Blink/Blink.ino
+++ Scripts/test/Blink/Blink.ino
@@ -7,7 +7,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
+  delay(500);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);                       // wait for a second
+  delay(500);                       // wait for a second
 }
