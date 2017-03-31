b76b6e1eecd1be9ff995c1e7b6aba0716debbd94
andxu <andxu@microsoft.com>
2017-03-31 13:40:25 +0800

    add version check @mac/

--- Scripts/src/tasks/task-arduinoide.js
+++ Scripts/src/tasks/task-arduinoide.js
@@ -2,12 +2,35 @@ import fs from 'fs-plus'
 import path from 'path'
 import * as util from '../util'
 import Arduino from '../arduino'
+import os from "os"
+import plist from 'plist'
 let arduino  = new Arduino();
+
+let parsePackageInfo = (file) => {
+    return plist.parse(fs.readFileSync(file, 'utf8'));
+};
 let checkArduinoVersion = async () => {
     try {
         await arduino.resolveArduinoPath();
-        if (fs.isFileSync(arduino.command) || fs.isFileSync(path.join(path.dirname(arduino.command), "lib/version.txt"))) {
-            return fs.readFileSync(path.join(path.dirname(arduino.command), "lib/version.txt"), 'utf-8') + ' @ ' + path.dirname(arduino.command);
+
+        if (fs.isFileSync(arduino.command)) {
+              const plat = os.platform();
+            if (plat === 'win32') {
+                if (fs.isFileSync(path.join(path.dirname(arduino.command), "lib/version.txt"))) {
+                    return fs.readFileSync(path.join(path.dirname(arduino.command), "lib/version.txt"), 'utf-8') + ' @ ' + path.dirname(arduino.command);
+                } else {
+                    throw new Error('cannnot find lib/version.txt in arduino installation path.');
+                }
+            } else if (plat === 'darwin') {
+                let current = path.dirname(arduino.command);
+                for (let i = 0; i < 3; i++) {
+                    if (fs.isFileSync(path.join(current, 'Info.plist'))) {
+                        return parsePackageInfo(path.join(current, 'Info.plist'), 'utf-8') + ' @ ' + current;
+                    }
+                    current = path.dirname(current);
+                }                
+                throw new Error('cannnot find Info.plist in arduino installation path.');
+            }       
         }
     } catch (error) {
         console.log(error);
