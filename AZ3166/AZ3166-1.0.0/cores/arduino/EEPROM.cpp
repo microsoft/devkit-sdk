d1a662620e0115e538747e64c164e0cbdf57e16a
Helen Li <heleli@microsoft.com>
2017-04-18 18:02:23 +0800

    Add getData API for Arduino EEPROM

--- AZ3166/AZ3166-1.0.0/cores/arduino/EEPROM.cpp
+++ AZ3166/AZ3166-1.0.0/cores/arduino/EEPROM.cpp
@@ -18,36 +18,32 @@
 
 #include "mbed.h"
 #include "EEPROM.h"
-#include "EEPROMInterface.h"
+#include "STSAFE_A.h"
+#include "STSAFE_A_I2C.h"
+
+#define STSAFE_A_POWER_PIN PB_14
+
+EEPROMClass::EEPROMClass()
+{
+    
+}
+
+EEPROMClass::~EEPROMClass()
+{
+
+}
 
 uint8_t EEPROMClass::read(int idx)
 {
     uint8_t zoneIndex = (idx & 0x7F000000) >> 24;
     uint16_t dataOffset = idx & 0x3FF;
 
-    int maxBuffLength = 0;
-
-    switch(zoneIndex)
-    {
-        case WIFI_SSID_ZONE_IDX:
-            maxBuffLength = WIFI_SSID_MAX_LEN;
-            break;
-        case WIFI_PWD_ZONE_IDX:
-            maxBuffLength = WIFI_PWD_MAX_LEN;
-            break;
-        case AZ_IOT_HUB_ZONE_IDX:
-            maxBuffLength = AZ_IOT_HUB_MAX_LEN;
-            break;
-        default:
-            maxBuffLength = EEPROM_DEFAULT_LEN;
-            break;
-    }
+    int maxBuffLength = getMaxLengthInZone(zoneIndex);
 
     if (dataOffset < maxBuffLength)
     {
-        EEPROMInterface eepromInterface;
         uint8_t outData;
-        int responseCode = eepromInterface.read(&outData, 1, dataOffset, zoneIndex);
+        int responseCode = eepromRead(&outData, 1, dataOffset, zoneIndex);
 
         if (responseCode > 0)
         {
@@ -56,4 +52,81 @@ uint8_t EEPROMClass::read(int idx)
     }
     
     return 0;
+}
+
+int EEPROMClass::getData(int idx, char* dataBuff, int buffSize)
+{
+    uint8_t zoneIndex = (idx & 0x7F000000) >> 24;
+
+    int maxBuffLength = getMaxLengthInZone(zoneIndex);
+
+    if (buffSize <= maxBuffLength)
+    {
+        uint8_t* outData = (uint8_t*)malloc(buffSize);
+        int result = eepromRead(outData, buffSize, 0, zoneIndex);
+
+        if (result == buffSize)
+        {
+            memcpy(dataBuff, outData, buffSize);
+            free(outData);
+            return result;
+        }
+    }
+    
+    return 0;
+}
+
+int EEPROMClass::eepromRead(uint8_t* dataBuff, int buffSize, uint16_t offset, uint8_t dataZoneIndex)
+{
+    if (dataBuff == NULL)
+    {
+        return -1;
+    }
+    
+    // Power on supply
+    DigitalOut STSAFE_A_Power(STSAFE_A_POWER_PIN);
+    STSAFE_A_Power = 1;
+    DELAY(50);
+
+    // Set up I2C
+	cSTSAFE_A_I2c STSAFE_A_I2c(I2C_SDA, I2C_SCL);
+	STSAFE_A_I2c.Frequency(400000);
+
+    // Configure I2C communication buffer
+	cSTSAFE_A Peripheral(&STSAFE_A_I2c, 0x20);
+	Peripheral.DataBufferConfiguration();
+
+    // Read
+	ReadBuffer* sts_read = NULL;
+	ResponseCode result = Peripheral.Read(0x00, 0x00, ALWAYS, dataZoneIndex, offset, buffSize, &sts_read, NO_MAC);
+	if (result == 0)
+	{
+        memcpy(dataBuff, sts_read->Data, sts_read->Length);
+        return sts_read->Length;
+	}
+    
+    return -1;
+}
+
+int getMaxLengthInZone(uint8_t zoneIndex)
+{
+    int length = 0;
+
+    switch(zoneIndex)
+    {
+        case WIFI_SSID_ZONE_IDX:
+            length = WIFI_SSID_MAX_LEN;
+            break;
+        case WIFI_PWD_ZONE_IDX:
+            length = WIFI_PWD_MAX_LEN;
+            break;
+        case AZ_IOT_HUB_ZONE_IDX:
+            length = AZ_IOT_HUB_MAX_LEN;
+            break;
+        default:
+            length = EEPROM_DEFAULT_LEN;
+            break;
+    }
+
+    return length;
 }
--- AZ3166/AZ3166-1.0.0/cores/arduino/EEPROM.h
+++ AZ3166/AZ3166-1.0.0/cores/arduino/EEPROM.h
@@ -20,16 +20,33 @@
 #define EEPROM_h
 
 #include "Arduino.h"
-#include "EEPROMInterface.h"
 
 #define WIFI_SSID_ADDRESS （WIFI_SSID_ZONE_IDX << 24)
 #define WIFI_PWD_ADDRESS (WIFI_PWD_ZONE_IDX << 24)
 #define AZ_IOTCONN_ADDRESS (AZ_IOT_HUB_ZONE_IDX << 24)
 
-struct EEPROMClass
+#define WIFI_SSID_ZONE_IDX      0x03
+#define WIFI_PWD_ZONE_IDX       0X0A
+#define AZ_IOT_HUB_ZONE_IDX     0X05
+
+#define WIFI_SSID_MAX_LEN       32
+#define WIFI_PWD_MAX_LEN        64
+#define AZ_IOT_HUB_MAX_LEN      200
+#define EEPROM_DEFAULT_LEN		200
+
+class EEPROMClass
 {
+  public:
+    EEPROMClass();
+    ~EEPROMClass();
     uint8_t read(int idx);
+    int getData(int idx, char* dataBuff, int buffSize);
+  
+  private:
+    int eepromRead(uint8_t* dataBuff, int buffSize, uint16_t offset, uint8_t dataZoneIndex);
 };
+
+int getMaxLengthInZone(uint8_t zoneIndex);
 
 static EEPROMClass EEPROM;
 
