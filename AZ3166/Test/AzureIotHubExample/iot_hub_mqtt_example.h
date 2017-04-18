ed0a030352f808fdaa81854e4f504fa1e20b3524
Arthur Ma <arthma@microsoft.com>
2017-04-18 20:30:53 +0800

    Update the IoT-Cognitive, add test sample for Iot hub mqtt

---
+++ AZ3166/Test/AzureIotHubExample/iot_hub_mqtt_example.h
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef __IOT_HUB_MQTT_EXAMPLE_H__
+#define __IOT_HUB_MQTT_EXAMPLE_H__
+
+
+
+	void iot_hub_mqtt_example_init(void);
+	void iot_hub_mqtt_example_send_event(const unsigned char *text);
+	void iot_hub_mqtt_example_loop(void);
+	
+
+
+#endif /* __IOT_HUB_MQTT_EXAMPLE_H__ */
