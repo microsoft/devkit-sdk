f791b31fe6cfd3fce8c931776f23fec93fef3662
Zhe Li <zlhe@microsoft.com>
2017-06-09 15:47:41 +0800

    add comment for oled draw method

--- AZ3166/AZ3166-1.0.0/cores/arduino/OledDisplay.h
+++ AZ3166/AZ3166-1.0.0/cores/arduino/OledDisplay.h
@@ -35,6 +35,23 @@ public:
     
     virtual int print(const char *s, bool wrap = false);
     virtual int print(unsigned int line, const char *s, bool wrap = false);
+
+    /**
+    * @brief show BMP image in OLED specified place
+    *
+    * @param x0: Position the X axis of the top left corner of the area image to display,
+    *            valid value is [0, 127]
+    * @param y0: Position the Y axis of the top left corner of the area image to display,
+    *            valid value is [0, 7]
+    * @param x1: Position the X axis of the bottom right corner of the area image to display,
+    *            valid value is [1, 128]
+    * @param y1: Position the Y axis of the bottom right corner of the area image to display,
+    *            valid value is [1, 8]
+    * @param BMP: BMP image pixel byte array. Every array element is an 8-bit binary data that
+    *             draws 8-connected pixels in the same column
+    * 
+    * @return none
+    */
     virtual int draw(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, unsigned char BMP[]);
         
 private:
