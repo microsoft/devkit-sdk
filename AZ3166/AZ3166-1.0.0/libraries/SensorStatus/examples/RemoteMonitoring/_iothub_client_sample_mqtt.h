d7ce1922917df045946a08ade496e9333c4f1890
Zhe Li <Sneezry@users.noreply.github.com>
2017-06-13 09:26:41 +0800

    Add remote monitoring example (#354)
    * add remote monitoring example
    
    * improve RemoteMonitoring.ino

---
+++ AZ3166/AZ3166-1.0.0/libraries/SensorStatus/examples/RemoteMonitoring/_iothub_client_sample_mqtt.h
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef IOTHUB_CLIENT_SAMPLE_MQTT_H
+#define IOTHUB_CLIENT_SAMPLE_MQTT_H
+
+void iothub_client_sample_mqtt_init(void);
+void iothub_client_sample_send_event(const unsigned char *text);
+void iothub_client_sample_mqtt_loop(void);
+void iothub_client_sample_mqtt_close(void);
+
+#endif /* IOTHUB_CLIENT_SAMPLE_MQTT_H */
