e0aaf50911e7f48366d718a148bdccc0bd5abf13
Renhe Li <renhe.li@hotmail.com>
2017-06-26 17:31:53 +0800

    update bearer token of js function (#386)

--- AZ3166/src/libraries/AzureIoT/examples/ShakeShake/azureFunction/index.js
+++ AZ3166/src/libraries/AzureIoT/examples/ShakeShake/azureFunction/index.js
@@ -34,7 +34,7 @@ module.exports = function (context, myEventHubMessage) {
                 let options = {
                     url: 'https://api.twitter.com/1.1/search/tweets.json?count=3&q=%23' + myEventHubMessage.topic,
                     headers: {
-                        'Authorization': 'Bearer ' + '{twitter bearer token}'
+                        'Authorization': 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAAGVU0AAAAAAAucpxA9aXc2TO6rNMnTcVit1P3YM%3DrQpyFeQ6LOwyvy7cqW5djhLPnFfjEK8H3hA1qfGDh93JRbI1le'
                     }
                 };
                 request(options, (error, response, body) => {
