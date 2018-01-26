c843f0a811289d7c027534a76c3b4c68120d09a1
Juan Manuel Servera <jmservera@outlook.com>
2018-01-26 14:38:58 +0100

    Use V2 for telemetry as well

--- AZ3166/src/libraries/AzureIoT/examples/RemoteMonitoringv2/RemoteMonitoringV2.ino
+++ AZ3166/src/libraries/AzureIoT/examples/RemoteMonitoringv2/RemoteMonitoringV2.ino
@@ -191,7 +191,7 @@ void setup() {
   {
     // Microsoft collects data to operate effectively and provide you the best experiences with our products. 
     // We collect data about the features you use, how often you use them, and how you use them.
-    send_telemetry_data_async("", "RemoteMonitoringSetup", "");
+    send_telemetry_data_async("", "RemoteMonitoringSetupV2", "");
   }
 }
 
