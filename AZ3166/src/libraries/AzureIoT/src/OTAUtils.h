90e260a1a5d79a320bdcf1f6370bb0fb4a3a3f1b
Suizhan Zheng <t-suzhen@microsoft.com>
2018-06-28 16:08:37 +0800

    delete commented include

--- AZ3166/src/libraries/AzureIoT/src/OTAUtils.h
+++ AZ3166/src/libraries/AzureIoT/src/OTAUtils.h
@@ -1,10 +1,7 @@
 #ifndef __OTA_UTILS_H__
 #define __OTA_UTILS_H__
 #include "SystemWiFi.h"
-// #include "Telemetry.h"
-// #include "DevkitDPSClient.h"
 #include "iothub_client_hsm_ll.h"
-// #include "SystemVersion.h"
 
 #ifdef __cplusplus
 extern "C"
