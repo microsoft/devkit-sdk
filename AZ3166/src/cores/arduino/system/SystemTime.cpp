ab80d46c664f263e5643ab74b3a2d94789f753f1
Arthur Ma <arthma@microsoft.com>
2019-03-20 18:30:33 +0800

    Provide new API to support customized time server

--- AZ3166/src/cores/arduino/system/SystemTime.cpp
+++ AZ3166/src/cores/arduino/system/SystemTime.cpp
@@ -6,16 +6,64 @@
 #include "SystemWiFi.h"
 #include "NTPClient.h"
 
-static const char* ntpHost[] = 
-{
-    "pool.ntp.org",
-    "cn.pool.ntp.org",
-    "europe.pool.ntp.org",
-    "asia.pool.ntp.org",
-    "oceania.pool.ntp.org"
-};
+static const char *defaultNTPs = "pool.ntp.org;cn.pool.ntp.org;europe.pool.ntp.org;asia.pool.ntp.org;oceania.pool.ntp.org";
+
+static char **ntpHosts = NULL;
+static int ntpHostCount = 0;
 
 static bool timeSynced = false;
+
+char **splitString(const char * tsList)
+{
+    int num = 1;
+    for (char* c = (char*)tsList; *c != 0; c++)
+    {
+        if (*c == ';')
+        {
+            num++;
+        }
+    }
+
+    char **list = (char**)calloc(num, sizeof(char*));
+    if (list == NULL)
+    {
+        return NULL;
+    }
+    list[0] = strdup(tsList);
+    int i = 1;
+    for (char* c = list[0]; *c != 0; c++)
+    {
+        if (*c == ';')
+        {
+            *c = 0;
+            list[i] = c + 1;
+            i++;
+        }
+    }
+        
+    ntpHostCount = num;
+    ntpHosts = list;
+    return ntpHosts;
+}
+
+int SetTimeServer(const char * tsList)
+{
+    if (tsList == NULL || tsList[0] == 0)
+    {
+        return -1;
+    }
+    
+    if (ntpHosts != NULL)
+    {
+        free(ntpHosts[0]);
+        free(ntpHosts);
+        ntpHosts = NULL;
+        ntpHostCount = 0;
+    }
+    splitString(tsList);
+
+    return 0;
+}
 
 static NTPResult NTPSyncUP(char* host)
 {
@@ -25,21 +73,23 @@ static NTPResult NTPSyncUP(char* host)
 
 void SyncTime(void)
 {
-    for (size_t i = 0; i < sizeof(ntpHost) / sizeof(ntpHost[0]); i++)
+    if (ntpHosts == NULL)
     {
-        if (NTPSyncUP((char*)ntpHost[i]) == NTP_OK)
+        splitString(defaultNTPs);
+    }
+    
+    timeSynced = false;
+    for (int i = 0; i < ntpHostCount; i++)
+    {
+        if (NTPSyncUP(ntpHosts[i]) == NTP_OK)
         {
-            if (!timeSynced)
-            {
-                time_t t = time(NULL);
-                Serial.printf("Time from %s, now is (UTC): %s\r\n", ntpHost[i], ctime(&t));
-                timeSynced = true;
-            }
+            timeSynced = true;
             break;
         }
-        else
-        {
-            Serial.printf("Unable to get the NTP host %s\r\n", ntpHost[i]);
-        }
     }
+}
+
+int IsTimeSynced(void)
+{
+    return (timeSynced ? 0 : -1);
 }
