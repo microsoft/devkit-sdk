0183e4bf24ba8220f08d02a6a42123acdb8d51bd
Arthur Ma <arthma@microsoft.com>
2019-08-10 01:29:37 +0800

    Support symmetric key setting in ap mode. (#1020)

--- AZ3166/src/cores/arduino/httpserver/app_httpd.cpp
+++ AZ3166/src/cores/arduino/httpserver/app_httpd.cpp
@@ -44,10 +44,13 @@
 #define DEFAULT_PAGE_SIZE (10*1024)
 
 static const char * page_head = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\"><title>AZ3166 WiFi Config</title><style>@charset \"UTF-8\";/*Flavor name:Default (mini-default)Author:Angelos Chalaris (chalarangelo@gmail.com)Maintainers:Angelos Chalarismini.css version:v2.1.5 (Fermion)*//*Browsers resets and base typography.*/html{font-size:16px;}html, *{font-family:-apple-system, BlinkMacSystemFont,\"Segoe UI\",\"Roboto\", \"Droid Sans\",\"Helvetica Neue\", Helvetica, Arial, sans-serif;line-height:1.5;-webkit-text-size-adjust:100%;}*{font-size:1rem;}body{margin:0;color:#212121;background:#f8f8f8;}section{display:block;}input{overflow:visible;}[type=\"radio\"]{position:absolute;left:-2rem;}h1, h2{line-height:1.2em;margin:0.75rem 0.5rem;font-weight:500;}h2 small{color:#424242;display:block;margin-top:-0.25rem;}h1{font-size:2rem;}h2{font-size:1.6875rem;}p{margin:0.5rem;}small{font-size:0.75em;}a{color:#0277bd;text-decoration:underline;opacity:1;transition:opacity 0.3s;}a:visited{color:#01579b;}a:hover, a:focus{opacity:0.75;}/*Definitions for the grid system.*/.container{margin:0 auto;padding:0 0.75rem;}.row{box-sizing:border-box;display:-webkit-box;-webkit-box-flex:0;-webkit-box-orient:horizontal;-webkit-box-direction:normal;display:-webkit-flex;display:flex;-webkit-flex:0 1 auto;flex:0 1 auto;-webkit-flex-flow:row wrap;flex-flow:row wrap;}[class^='col-sm-']{box-sizing:border-box;-webkit-box-flex:0;-webkit-flex:0 0 auto;flex:0 0 auto;padding:0 0.25rem;}.col-sm-10{max-width:83.33333%;-webkit-flex-basis:83.33333%;flex-basis:83.33333%;}.col-sm-offset-1{margin-left:8.33333%;}@media screen and (min-width:768px){.col-md-4{max-width:33.33333%;-webkit-flex-basis:33.33333%;flex-basis:33.33333%;}.col-md-offset-4{margin-left:33.33333%;}}/*Definitions for navigation elements.*/header{display:block;height:2.75rem;background:#1e6bb8;color:#f5f5f5;padding:0.125rem 0.5rem;white-space:nowrap;overflow-x:auto;overflow-y:hidden;}header .logo{color:#f5f5f5;font-size:1.35rem;line-height:1.8125em;margin:0.0625rem 0.375rem 0.0625rem 0.0625rem;transition:opacity 0.3s;}header .logo{text-decoration:none;}/*Definitions for forms and input elements.*/form{background:#eeeeee;border:1px solid #c9c9c9;margin:0.5rem;padding:0.75rem 0.5rem 1.125rem;}.input-group{display:inline-block;margin-left:2rem;position:relative;}.input-group.fluid{display:-webkit-box;-webkit-box-pack:justify;display:-webkit-flex;display:flex;-webkit-align-items:center;align-items:center;-webkit-justify-content:center;justify-content:center;}.input-group.fluid>input:not([type=\"radio\"]),.input-group.fluid>textarea{-webkit-box-flex:1;width:100%;-webkit-flex-grow:1;flex-grow:1;-webkit-flex-basis:0;flex-basis:0;}@media screen and (max-width:767px){.input-group.fluid{-webkit-box-orient:vertical;-webkit-align-items:stretch;align-items:stretch;-webkit-flex-direction:column;flex-direction:column;}}[type=\"password\"],[type=\"text\"],select,textarea{width:100%;box-sizing:border-box;background:#fafafa;color:#212121;border:1px solid #c9c9c9;border-radius:2px;margin:0.25rem 0;padding:0.5rem 0.75rem;}input:not([type=\"button\"]):not([type=\"submit\"]):not([type=\"reset\"]):hover, input:not([type=\"button\"]):not([type=\"submit\"]):not([type=\"reset\"]):focus, select:hover, select:focus{border-color:#0288d1;box-shadow:none;}input:not([type=\"button\"]):not([type=\"submit\"]):not([type=\"reset\"]):disabled, select:disabled{cursor:not-allowed;opacity:0.75;}::-webkit-input-placeholder{opacity:1;color:#616161;}::-moz-placeholder{opacity:1;color:#616161;}::-ms-placeholder{opacity:1;color:#616161;}::placeholder{opacity:1;color:#616161;}button::-moz-focus-inner, [type=\"submit\"]::-moz-focus-inner{border-style:none;padding:0;}button, [type=\"submit\"]{-webkit-appearance:button;}button{overflow:visible;text-transform:none;}button, [type=\"submit\"], a.button, .button{display:inline-block;background:rgba(208, 208, 208, 0.75);color:#212121;border:0;border-radius:2px;padding:0.5rem 0.75rem;margin:0.5rem;text-decoration:none;transition:background 0.3s;cursor:pointer;}button:hover, button:focus, [type=\"submit\"]:hover, [type=\"submit\"]:focus, a.button:hover, a.button:focus, .button:hover, .button:focus{background:#d0d0d0;opacity:1;}button:disabled, [type=\"submit\"]:disabled, a.button:disabled, .button:disabled{cursor:not-allowed;opacity:0.75;}/*Custom elements for forms and input elements.*/button.primary, [type=\"submit\"].primary, .button.primary{background:rgba(30, 107, 184, 0.9);color:#fafafa;}button.primary:hover, button.primary:focus, [type=\"submit\"].primary:hover, [type=\"submit\"].primary:focus, .button.primary:hover, .button.primary:focus{background:#0277bd;}#content{margin-top:2em;} table, th, td {border:1px solid #c9c9c9; border-collapse:collapse;} th, td {padding:5px;padding-left:10px} td {text-align: left;} th {background-color:#EEEEEE;color: #616161;} tr {background-color: #EEEEEE;color: #616161;}</style></head>";
-static const char * wifi_setting_a = "<body><header><h1 class=\"logo\">IoT DevKit Settings</h1></header><section class=\"container\"><div id=\"content\" class=\"row\"><div class=\"col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-4\" style=\"text-align:center;\"><form action=\"result\" method=\"post\" enctype=\"multipart/form-data\"><div class=\"input-group fluid\"><input type=\"radio\" name=\"input_ssid_method\" value=\"select\" onclick=\"changeSSIDInput()\" checked><select name=\"SSID\" id=\"SSID-select\"> ";
-static const char * wifi_setting_b = "</select></div><div class=\"input-group fluid\"><input type=\"radio\" name=\"input_ssid_method\" value=\"text\" onclick=\"changeSSIDInput()\"><input type=\"text\" id=\"SSID-text\" placeholder=\"SSID\" disabled></div><div class=\"input-group fluid\"><input type=\"password\" value=\"\" name=\"PASS\" id=\"password\" placeholder=\"Password\"></div>";
+static const char * wifi_setting_a = "<body><header><h1 class=\"logo\">IoT DevKit Settings</h1></header><section class=\"container\"><div id=\"content\" class=\"row\"><div class=\"col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-4\" style=\"text-align:center;\"><form action=\"result\" method=\"post\" enctype=\"multipart/form-data\"><div><fieldset> <legend>Wi-Fi Settings</legend><div class=\"input-group fluid\"><input type=\"radio\" name=\"input_ssid_method\" value=\"select\" onclick=\"changeSSIDInput()\" checked><select name=\"SSID\" id=\"SSID-select\"> ";
+static const char * wifi_setting_b = "</select></div><div class=\"input-group fluid\"><input type=\"radio\" name=\"input_ssid_method\" value=\"text\" onclick=\"changeSSIDInput()\"><input type=\"text\" id=\"SSID-text\" placeholder=\"SSID\" disabled></div><div class=\"input-group fluid\"><input type=\"password\" value=\"\" name=\"PASS\" id=\"password\" placeholder=\"Password\"></div></fieldset></div>";
+static const char * iot_setting_a = "<div><fieldset><legend>Azure IoT Settings</legend>";
 static const char * device_conn_setting = "<div class=\"input-group fluid\"><input type=\"text\" name=\"DeviceConnectionString\" id=\"DeviceConnectionString\" placeholder=\"IoT Device Connection String\"></div>";
 static const char * cert_setting = "<div class=\"input-group fluid\"><textarea name=\"certificate\" rows=\"5\" placeholder=\"X.509 Certificate\"></textarea></div>";
+static const char * dps_symmetric_key = "<div class=\"input-group fluid\"><input type=\"text\" name=\"DPSEndpoint\" id=\"DPSEndpoint\" placeholder=\"The DPS endpoint\" value=\"global.azure-devices-provisioning.net\"></div><div class=\"input-group fluid\"><input type=\"text\" name=\"ScopeId\" id=\"ScopeId\" placeholder=\"The DPS ID Scope\"></div><div class=\"input-group fluid\"><input type=\"text\" name=\"RegistrationId\" id=\"RegistrationId\" placeholder=\"The Registration ID\"></div><div class=\"input-group fluid\"><input type=\"text\" name=\"SymmetricKey\" id=\"SymmetricKey\" placeholder=\"The symmetric key\"></div>";
+static const char * iot_setting_b = "</fieldset></div>";
 static const char * setting_end = "<div class=\"input-group fluid\"><button type=\"submit\" class=\"primary\">Save</button></div></form><h5 style=\"color:#616161;\">Please refresh this page to update SSID if you cannot find it from the list</h5></div></div></section><script>function changeSSIDInput(){var inputFromSelect=document.getElementsByName(\"input_ssid_method\")[0].checked;var select=document.getElementById(\"SSID-select\");var text=document.getElementById(\"SSID-text\");if(inputFromSelect){select.name=\"SSID\";select.removeAttribute(\"disabled\");text.name=\"\";text.setAttribute(\"disabled\",\"\")}else{select.name=\"\";select.setAttribute(\"disabled\",\"\");text.name=\"SSID\";text.removeAttribute(\"disabled\")}};</script></body></html>";
 
 static const char * result_head = "<body><header> <h1 class=\"logo\">IoT DevKit Settings</h1></header><section class=\"container\"> <div id=\"content\" class=\"row\"> <div class=\"col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-4\" style=\"text-align:center;\"><table align=\"center\" style=\"width:80%\"><tr><th>Settings</td></tr>";
@@ -228,17 +231,33 @@ static int web_system_setting_page(httpd_request_t *req)
     }
     strcpy(&setting_page[len], wifi_setting_b);
     len += strlen(wifi_setting_b);
-    if (web_settings & WEB_SETTING_IOT_DEVICE_CONN_STRING)
+    if (web_settings)
     {
-        // Enable IoT Device Connection string
-        strcpy(&setting_page[len], device_conn_setting);
-        len += strlen(device_conn_setting);
-    }
-    if (web_settings & WEB_SETTING_IOT_CERT)
-    {
-        // Enable X.509 cert
-        strcpy(&setting_page[len], cert_setting);
-        len += strlen(cert_setting);
+        strcpy(&setting_page[len], iot_setting_a);
+        len += strlen(iot_setting_a);
+        if (web_settings & WEB_SETTING_IOT_DPS_SYMMETRIC_KEY)
+        {
+            // Enable DPS symmetric key
+            strcpy(&setting_page[len], dps_symmetric_key);
+            len += strlen(dps_symmetric_key);
+        }
+        else
+        {
+            if (web_settings & WEB_SETTING_IOT_DEVICE_CONN_STRING)
+            {
+                // Enable IoT Device Connection string
+                strcpy(&setting_page[len], device_conn_setting);
+                len += strlen(device_conn_setting);
+            }
+            if (web_settings & WEB_SETTING_IOT_CERT)
+            {
+                // Enable X.509 cert
+                strcpy(&setting_page[len], cert_setting);
+                len += strlen(cert_setting);
+            }
+        }
+        strcpy(&setting_page[len], iot_setting_b);
+        len += strlen(iot_setting_b);
     }
     strcat(&setting_page[len], setting_end);
     len += strlen(setting_end) + 1;
@@ -276,15 +295,24 @@ int web_system_setting_result_page(httpd_request_t *req)
     memset(value_pass, 0, sizeof(value_pass));
 
     buf_size = 512 + WIFI_SSID_MAX_LEN + WIFI_PWD_MAX_LEN;
-    if (web_settings & WEB_SETTING_IOT_DEVICE_CONN_STRING)
+    if (web_settings & WEB_SETTING_IOT_DPS_SYMMETRIC_KEY)
     {
+        value_x509 = (char*)calloc(AZ_IOT_HUB_MAX_LEN / 4 + 1, 1);  // Temp buffer
         value_device_connection_string = (char*)calloc(AZ_IOT_HUB_MAX_LEN + 1, 1);
         buf_size += AZ_IOT_HUB_MAX_LEN;
     }
-    if (web_settings & WEB_SETTING_IOT_CERT)
+    else
     {
-        value_x509 = (char*)calloc(AZ_IOT_X509_MAX_LEN + 1, 1);
-        buf_size += AZ_IOT_X509_MAX_LEN;
+        if (web_settings & WEB_SETTING_IOT_DEVICE_CONN_STRING)
+        {
+            value_device_connection_string = (char*)calloc(AZ_IOT_HUB_MAX_LEN + 1, 1);
+            buf_size += AZ_IOT_HUB_MAX_LEN;
+        }
+        if (web_settings & WEB_SETTING_IOT_CERT)
+        {
+            value_x509 = (char*)calloc(AZ_IOT_X509_MAX_LEN + 1, 1);
+            buf_size += AZ_IOT_X509_MAX_LEN;
+        }
     }
     buf = (char *)calloc(buf_size, 1);
     if (buf == NULL)
@@ -308,16 +336,38 @@ int web_system_setting_result_page(httpd_request_t *req)
         err = httpd_get_tag_from_multipart_form(buf, boundary, "PASS", value_pass, WIFI_PWD_MAX_LEN);
         require_noerr(err, _exit);
 
-        if (value_device_connection_string)
+        if (web_settings & WEB_SETTING_IOT_DPS_SYMMETRIC_KEY)
         {
-            err = httpd_get_tag_from_multipart_form(buf, boundary, "DeviceConnectionString", value_device_connection_string, AZ_IOT_HUB_MAX_LEN);
+            strcpy(value_device_connection_string, "DPSEndpoint=");
+            err = httpd_get_tag_from_multipart_form(buf, boundary, "DPSEndpoint", value_x509, AZ_IOT_HUB_MAX_LEN / 4);
             require_noerr(err, _exit);
+            strcat(value_device_connection_string, value_x509);
+            strcat(value_device_connection_string, ";ScopeId=");
+            err = httpd_get_tag_from_multipart_form(buf, boundary, "ScopeId", value_x509, AZ_IOT_HUB_MAX_LEN / 4);
+            require_noerr(err, _exit);
+            strcat(value_device_connection_string, value_x509);
+            strcat(value_device_connection_string, ";RegistrationId=");
+            err = httpd_get_tag_from_multipart_form(buf, boundary, "RegistrationId", value_x509, AZ_IOT_HUB_MAX_LEN / 4);
+            require_noerr(err, _exit);
+            strcat(value_device_connection_string, value_x509);
+            strcat(value_device_connection_string, ";SymmetricKey=");
+            err = httpd_get_tag_from_multipart_form(buf, boundary, "SymmetricKey", value_x509, AZ_IOT_HUB_MAX_LEN / 4);
+            require_noerr(err, _exit);
+            strcat(value_device_connection_string, value_x509);
         }
-
-        if (value_x509)
+        else
         {
-            err = httpd_get_tag_from_multipart_form(buf, boundary, "certificate", value_x509, AZ_IOT_X509_MAX_LEN);
-            require_noerr(err, _exit);
+            if (value_device_connection_string)
+            {
+                err = httpd_get_tag_from_multipart_form(buf, boundary, "DeviceConnectionString", value_device_connection_string, AZ_IOT_HUB_MAX_LEN);
+                require_noerr(err, _exit);
+            }
+
+            if (value_x509)
+            {
+                err = httpd_get_tag_from_multipart_form(buf, boundary, "certificate", value_x509, AZ_IOT_X509_MAX_LEN);
+                require_noerr(err, _exit);
+            }
         }
     }
     else // Post data is URL encoded
@@ -328,16 +378,38 @@ int web_system_setting_result_page(httpd_request_t *req)
         err = httpd_get_tag_from_post_data(buf, "PASS", value_pass, WIFI_PWD_MAX_LEN);
         require_noerr(err, _exit);
 
-        if (value_device_connection_string)
+        if (web_settings & WEB_SETTING_IOT_DPS_SYMMETRIC_KEY)
         {
-            err = httpd_get_tag_from_post_data(buf, "DeviceConnectionString", value_device_connection_string, AZ_IOT_HUB_MAX_LEN);
+            strcpy(value_device_connection_string, "DPSEndpoint=");
+            err = httpd_get_tag_from_post_data(buf, "DPSEndpoint", value_x509, AZ_IOT_HUB_MAX_LEN / 4);
             require_noerr(err, _exit);
+            strcat(value_device_connection_string, value_x509);
+            strcat(value_device_connection_string, ";ScopeId=");
+            err = httpd_get_tag_from_post_data(buf, "ScopeId", value_x509, AZ_IOT_HUB_MAX_LEN / 4);
+            require_noerr(err, _exit);
+            strcat(value_device_connection_string, value_x509);
+            strcat(value_device_connection_string, ";RegistrationId=");
+            err = httpd_get_tag_from_post_data(buf, "RegistrationId", value_x509, AZ_IOT_HUB_MAX_LEN / 4);
+            require_noerr(err, _exit);
+            strcat(value_device_connection_string, value_x509);
+            strcat(value_device_connection_string, ";SymmetricKey=");
+            err = httpd_get_tag_from_post_data(buf, "SymmetricKey", value_x509, AZ_IOT_HUB_MAX_LEN / 4);
+            require_noerr(err, _exit);
+            strcat(value_device_connection_string, value_x509);
         }
-
-        if (value_x509)
+        else
         {
-            err = httpd_get_tag_from_post_data(buf, "certificate", value_x509, AZ_IOT_X509_MAX_LEN);
-            require_noerr(err, _exit);
+            if (value_device_connection_string)
+            {
+                err = httpd_get_tag_from_post_data(buf, "DeviceConnectionString", value_device_connection_string, AZ_IOT_HUB_MAX_LEN);
+                require_noerr(err, _exit);
+            }
+
+            if (value_x509)
+            {
+                err = httpd_get_tag_from_post_data(buf, "certificate", value_x509, AZ_IOT_X509_MAX_LEN);
+                require_noerr(err, _exit);
+            }
         }
     }
     // Free the data buffer
@@ -461,6 +533,10 @@ _exit:
     if (value_device_connection_string)
     {
         free(value_device_connection_string);
+    }
+    if (value_x509)
+    {
+        free(value_x509);
     }
     if (result_page)
     {
